f4581c848cbffaad7c36720d805a1c8d00a3aa8d
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-05 01:14:13 -0500

    fmt and lint

--- node-weather/src/index.ts
+++ node-weather/src/index.ts
@@ -1,31 +1,32 @@
-import { 
-  APIGatewayProxyEvent, 
-  APIGatewayEventRequestContext,
-  APIGatewayProxyResult
+import {
+  // APIGatewayProxyEvent,
+  // APIGatewayEventRequestContext,
+  APIGatewayProxyResult,
 } from "aws-lambda";
 import { fetchWeather } from "./meteo";
-  
-let response : APIGatewayProxyResult;
+
+let response: APIGatewayProxyResult;
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * ß
  */
-exports.lambdaHandler = async (event : APIGatewayProxyEvent , context: APIGatewayEventRequestContext) => {
+// exports.lambdaHandler = async (event : APIGatewayProxyEvent , context: APIGatewayEventRequestContext) => {
+exports.lambdaHandler = async () => {
   try {
     response = {
-      'statusCode': 200,
-      'body': JSON.stringify({
+      statusCode: 200,
+      body: JSON.stringify({
         message: await fetchWeather(),
-      })
+      }),
     };
   } catch (err) {
     console.log(err);
