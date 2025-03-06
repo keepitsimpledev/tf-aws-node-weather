e2ccc71cb7892df78dc0be473d0c6abdefe7eb36
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-05 20:42:20 -0500

    add tf apply to pipeline

--- ci-cd/deploy-lambda.sh
+++ ci-cd/deploy-lambda.sh
@@ -6,11 +6,10 @@ npm run build
 echo "END: build node artifacts"
 echo "BEGIN: packaging lambda"
 cp -r node_modules/ dist/src/
+mkdir ../tf-lambda/build
 cd dist/src/
-mkdir ../../../tf-lambda/build
 zip -r ../../../tf-lambda/build/lambda_weather_function.zip *
 echo "END: packaging lambda"
 cd ../../../tf-lambda
 ls build/
-#        terraform apply --auto-approve
-# TODO: figure out how to keep tf.state in AWS
+terraform apply --auto-approve
