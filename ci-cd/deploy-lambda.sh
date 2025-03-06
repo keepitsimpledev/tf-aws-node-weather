32d8062d0853a8594e6008caf07a4c2cef1f7f9b
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-05 22:54:49 -0500

    correct script

--- ci-cd/deploy-lambda.sh
+++ ci-cd/deploy-lambda.sh
@@ -5,7 +5,7 @@ cd node-weather
 npm run build
 echo "END: build node artifacts"
 # echo "BEGIN: packaging lambda"
-cp -r node_modules/dist/src/
+cp -r node_modules/ dist/src/
 # mkdir ../tf-lambda/build
 # cd dist/src/
 # zip -rq ../../../tf-lambda/build/lambda_weather_function.zip *
