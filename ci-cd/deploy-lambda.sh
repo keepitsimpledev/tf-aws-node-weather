27a6e05a18513c32c774616f5b7aa1ea5224e7c6
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-05 19:56:18 -0500

    test another script param

--- ci-cd/deploy-lambda.sh
+++ ci-cd/deploy-lambda.sh
@@ -13,6 +13,7 @@ echo $2
 
 if [ -z ${0+x} ]; then echo "0 is unset"; else echo "0 is set to '$0'"; fi
 if [ -z ${1+x} ]; then echo "1 is unset"; else echo "1 is set to '$1'"; fi
+if [ -z ${2+x} ]; then echo "2 is unset"; else echo "1 is set to '$2'"; fi
 
 # echo "BEGIN: build node artifacts"
 # cd node-weather
