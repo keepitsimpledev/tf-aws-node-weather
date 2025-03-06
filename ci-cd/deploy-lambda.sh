b6fbd5e8c15e991ff970d6dfc122d472b5d666d9
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-05 19:49:11 -0500

    call bash script in pipeline

---
+++ ci-cd/deploy-lambda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# TODO: check for AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+if [ -z ${AKI+x} ]; then echo "AKI is unset"; else echo "AKI is set to '$AKI'"; fi
+if [ -z ${SAK+x} ]; then echo "SAK is unset"; else echo "SAK is set to '$SAK'"; fi
+
+
+# echo "BEGIN: build node artifacts"
+# cd node-weather
+# npm run build
+# echo "END: build node artifacts"
+# echo "BEGIN: packaging lambda"
+# cp -r node_modules/ dist/src/
+# cd dist/src/
+# mkdir ../../../tf-lambda/build
+# zip -r ../../../tf-lambda/build/lambda_weather_function.zip *
+# echo "END: packaging lambda"
+# cd ../../../tf-lambda
+# ls build/
+#        terraform apply --auto-approve
+# TODO: figure out how to keep tf.state in AWS
