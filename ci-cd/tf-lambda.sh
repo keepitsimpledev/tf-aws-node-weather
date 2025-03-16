9d629bf2e4b76781cda8f4b60ed53233c9ed028d
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-15 21:06:39 -0400

    refactor pipelines, fix destroy pipeline

--- ci-cd/tf-lambda.sh
+++ ci-cd/tf-lambda.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" != "apply" ] && [ "$1" != "destroy" ];
+    then echo "expected param 'apply' or 'destroy' but got: '$1'" && exit 1;
+fi
+
 echo "BEGIN: build node artifacts"
 pushd node-weather/
 npm run build
@@ -7,9 +11,9 @@ cp -r node_modules/ dist/src/
 popd
 echo "END: build node artifacts"
 
-echo "BEGIN: terraform deploy"
+echo "BEGIN: terraform $1"
 pushd tf-lambda/
 terraform init # is it appropriate to have this here?
-terraform apply --auto-approve
+terraform $1 --auto-approve
 popd
-echo "END: terraform deploy"
+echo "END: terraform $1"
