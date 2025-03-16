8c759c16bc5b80d3b4730f16a9dc76f779a87805
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-15 21:20:29 -0400

    fix script

--- ci-cd/destroy-lambda.sh
+++ ci-cd/destroy-lambda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "BEGIN: terraform destory"
-mkdir node-weather/dist/src/ # dummy directory to appease terraform
+mkdir -p node-weather/dist/src/ # dummy directory to appease terraform
 pushd tf-lambda/
 terraform init
 terraform destory --auto-approve
