700724def4da2637da984050c034e7ff980af577
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-16 23:22:19 -0400

    adjust names

--- tf-lambda/elasticache.tf
+++ tf-lambda/elasticache.tf
@@ -1,7 +1,7 @@
 # example from: https://dev.to/giasuddin90/creating-an-aws-elasticache-redis-cluster-using-terraform-eb6
 
-resource "aws_security_group" "redis_sg" {
-  name        = "redis-security-group"
+resource "aws_security_group" "cache_sg" {
+  name        = "cache-security-group"
   description = "Security group for Redis cluster"
 
   ingress {
@@ -13,8 +13,8 @@ resource "aws_security_group" "redis_sg" {
 }
 
 # this take ~10m to apply, ~5m to destroy
-resource "aws_elasticache_cluster" "pte-dev-redis" {
-  cluster_id           = "pte-dev"
+resource "aws_elasticache_cluster" "project_cache" {
+  cluster_id           = "project_cache_id"
   engine               = "redis"
   node_type            = "cache.t3.micro"  # Choose a suitable instance type based on your needs
   num_cache_nodes      = 1
@@ -23,5 +23,5 @@ resource "aws_elasticache_cluster" "pte-dev-redis" {
   apply_immediately    = true
   port                 = 6379
 
-  security_group_ids   = [aws_security_group.redis_sg.id]  # Associate the Redis cluster with the custom security group
+  security_group_ids   = [aws_security_group.cache_sg.id]  # Associate the Redis cluster with the custom security group
 }
