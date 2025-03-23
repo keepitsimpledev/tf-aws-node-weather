cb86c2eeb966b1b0af5214e74aa24e81b3129e76
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-03-23 09:29:08 -0400

    try public subnet

--- tf-lambda/lambda.tf
+++ tf-lambda/lambda.tf
@@ -34,7 +34,7 @@ resource "aws_lambda_function" "weather_lambda" {
   runtime = "nodejs22.x"
 
   vpc_config {
-    subnet_ids         = [aws_subnet.subnet_private.id]
+    subnet_ids         = [aws_subnet.subnet_public.id]
     security_group_ids = [aws_security_group.cache_sg.id]
   }
 
