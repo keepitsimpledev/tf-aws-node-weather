8e9eed2ba3d58750d13f2571d4cc1a43adb5d16b
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-08-09 13:02:52 -0400

    test without iam attachment

--- tf-lambda/vpc.tf
+++ tf-lambda/vpc.tf
@@ -97,7 +97,7 @@ resource "aws_default_security_group" "default_security_group" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
+#   role       = aws_iam_role.iam_for_lambda.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+# }
