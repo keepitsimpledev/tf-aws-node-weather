e5272b3b9032fe6f31a4ea1eacdb6065938238c3
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-08-09 13:09:07 -0400

    remove unused policy attachment

--- tf-lambda/vpc.tf
+++ tf-lambda/vpc.tf
@@ -96,8 +96,3 @@ resource "aws_default_security_group" "default_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
-#   role       = aws_iam_role.iam_for_lambda.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-# }
