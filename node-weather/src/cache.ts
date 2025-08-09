ef6c3f9fa638d3aba0a82808d0afc388c043de48
keepitsimpledev <keep.it.simple.dev@gmail.com>
2025-08-09 11:33:21 -0400

    fmt

--- node-weather/src/cache.ts
+++ node-weather/src/cache.ts
@@ -43,12 +43,12 @@ export async function getPayload(): Promise<string> {
   let response: string | null = await getValue(cacheKey);
 
   if (null === response) {
-    console.log('fetching meteo data');
+    console.log("fetching meteo data");
     const weatherData = await fetchWeather();
     response = JSON.stringify(weatherData);
     await setValue(cacheKey, response);
   } else {
-    console.log('using cached data');
+    console.log("using cached data");
   }
 
   return response;
